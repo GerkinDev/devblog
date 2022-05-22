4950888b80815dfdff63604b4da4843ecf386db3
Gerkin <alexandre.j.p.germain@gmail.com>
2022-05-22 15:51:57 +0200

    perf: keep cache across builds on local

--- oci-run.sh
+++ oci-run.sh
@@ -1,9 +1,11 @@
+! [ -d $(pwd)/.cache ] && mkdir $(pwd)/.cache
 $(
   ( which podman &> /dev/null && echo 'podman' ) ||
   ( which docker &> /dev/null && echo 'docker' ) ||
   ( >&2 echo 'Missing podman or docker' && exit 1 )
 ) run --rm -it \
   -v $(pwd):/src \
+  -v $(pwd)/.cache:/tmp \
   -p 1313:1313 \
   --name hugo-devblog \
   klakegg/hugo:ext-alpine \
