b7002d59eb81829706160deec7cd7f03ee51e03d
GerkinDev <alexandre.j.p.germain@gmail.com>
2026-02-22 01:05:52 +0100

    chore: rework code wrapper rename

--- assets/scripts/custom-code-wrappers.ts
+++ assets/scripts/custom-code-wrappers.ts
@@ -1,17 +1,6 @@
-const rewriteIncludedFile = (highlightBlock: HTMLElement) => {
-	const section = highlightBlock.parentElement;
-	const label = section.getAttribute('data-label');
-	const codeContainer = highlightBlock.querySelector('.chroma td:nth-child(2) code');
-	if(!codeContainer){
-		return console.warn('Can\'t get code container', highlightBlock);
-	}
-	codeContainer.setAttribute('data-lang', label);
-}
-const rewriteHighlightBlock = (highlightBlock: HTMLElement) => {
-	if(highlightBlock.parentElement?.classList.contains('included-file')){
-		rewriteIncludedFile(highlightBlock)
-	}
-}
-
-Array.from( document.querySelectorAll<HTMLElement>( '.highlight' ))
-	.forEach(rewriteHighlightBlock);
+Array.from( document.querySelectorAll<HTMLElement>( '[data-code-block-label-override]' ))
+	.forEach(element => {
+		const codeContainer = element.querySelector('.chroma td:nth-child(2) code');
+		const label = element.getAttribute('data-code-block-label-override');
+		codeContainer.setAttribute('data-lang', label);
+	});
